5bcf30adf2eb1a46bfe57ce8cbf37e3498004769
unknown <boss.man.development@gmail.com>
2021-08-19 16:03:25 -0500

    Updated web pages

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 //jshint esversion:6
 
 //Starting Code/ BoilerPlate Code
+require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');       // Lvl 2 encription(added code)
 
 const app = express();
 
@@ -16,13 +18,19 @@ app.use(express.static("public"));
 
 // Database Code
 mongoose.connect("mongodb://localhost:27017/userDB", {
-    useNewUrlParser: true, useUnifiedTopology: true
+  useNewUrlParser: true, useUnifiedTopology: true
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({                // Lvl 2 encription(altered code)
     email: String,
     password: String
-};
+});
+  
+// Encryption
+userSchema.plugin(encrypt, {                            // Lvl 2 encription(added code)
+    secret: process.env.SECRET, 
+    encryptedFields: ["password"]                       // only encrypts specific fields in array
+});             
 
 const User = new mongoose.model("User", userSchema);
 
@@ -62,7 +70,7 @@ app.route("/register")
             email: req.body.username,
             password: req.body.password
         });
-
+    
         newUser.save(function(err){
             if (err){
                 console.log(err);
@@ -74,5 +82,5 @@ app.route("/register")
 
 // Server Connection Code
 app.listen(3000, function() {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
